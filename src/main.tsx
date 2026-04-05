2e67ffc3c9abb7d37348102ae51db17e73a6341d
Chris Poulter <cpoulter@hotmail.co.uk>
2026-04-05 10:27:21 +0100

    fix: update Toaster component to use invert prop for improved visibility

--- src/main.tsx
+++ src/main.tsx
@@ -29,7 +29,7 @@ createRoot(document.getElementById("root")!).render(
           <App />
         </ThemeProvider>
       </BrowserRouter>
-      <Toaster />
+      <Toaster invert />
     </QueryClientProvider>
   </StrictMode>
 );
