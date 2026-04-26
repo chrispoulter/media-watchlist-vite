65b4ce9fce3b794ec1718f42464a46a5c4406d0b
Chris Poulter <cpoulter@hotmail.co.uk>
2026-04-26 11:36:35 +0100

    refactor: add console log for import.meta.env in config schema

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -10,6 +10,8 @@ const configSchema = z.object({
   VITE_API_URL: z.url("VITE_API_URL must be a valid URL"),
 });
 
+console.log(import.meta.env)
+
 export const config = configSchema.parse({
   ...window.__ENV__,
   ...import.meta.env,
